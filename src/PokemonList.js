65f80d45621182703179279a4f11095e781dd4ad
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-15 11:03:50 -0800

    created PokemonList function that maps through pokemon array

---
+++ src/PokemonList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Pokemon from './Pokemon';
+
+export default function PokemonList({ pokemon }) {
+  return (
+    <div>
+      {pokemon.map((pokemon, i) => <Pokemon key={`${pokemon._id}-${i}`} pokemon={pokemon} />)}
+    </div>
+  );
+}
