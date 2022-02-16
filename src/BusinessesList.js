b409a049bf4cfc742f53415943f7c2711a6b5c94
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-15 16:49:28 -0800

    added css styling to the yelp section

--- src/BusinessesList.js
+++ src/BusinessesList.js
@@ -3,16 +3,16 @@ import BusinessType from './BusinessType';
 
 export default function BusinessesList({ businessesArr }) {
   return (
-    <div>
+    <div className='business-card'>
       {
         businessesArr.map((yelpItem, i) =>
-          <div key={yelpItem + i}> 
+          <div key={yelpItem + i} className='business-item'> 
             <h3>{yelpItem.name}</h3>
             <p>{yelpItem.location.display_address}</p>
-            <p>{yelpItem.rating}⭐</p>
             <div>
               <BusinessType categories={yelpItem.categories}/>
             </div>
+            <p>{yelpItem.rating}⭐</p>
             <img src={yelpItem.image_url} style={{ width: '200px' }}/>
           </div>)
       }
